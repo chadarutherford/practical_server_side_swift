3326d8e9881018321c69c0abeb0cec0f4bbcffbf
Chad Rutherford <chad_rutherford@outlook.com>
2020-07-01 15:37:08 -0400

    Edited Blog Post Admin Controller

---
+++ Sources/App/Form/BasicFormField.swift
@@ -0,0 +1,13 @@
+//
+//  BasicFormField.swift
+//  
+//
+//  Created by Chad Rutherford on 7/1/20.
+//
+
+import Foundation
+
+struct BasicFormField: Encodable {
+	var value: String = ""
+	var error: String?
+}
