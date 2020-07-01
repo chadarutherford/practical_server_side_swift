f4d194d756ad43ebe45edae5bfa61937c609a016
Chad Rutherford <chad_rutherford@outlook.com>
2020-07-01 12:20:11 -0400

    Added User capabilities to configure

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,11 +8,15 @@ import Vapor
 public func configure(_ app: Application) throws {
 	// uncomment to serve files from /Public folder
 	app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+	app.sessions.use(.fluent)
+	app.migrations.add(SessionRecord.migration)
+	app.middleware.use(app.sessions.middleware)
 	app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 	app.views.use(.leaf)
 	app.leaf.cache.isEnabled = app.environment.isRelease
 	
 	let modules: [Module] = [
+		UserModule(),
 		FrontendModule(),
 		BlogModule(),
 	]
