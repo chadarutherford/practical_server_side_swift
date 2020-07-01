761c6b7b2e46e3c507453302ad0826e0fb0a4df0
Chad Rutherford <chad_rutherford@outlook.com>
2020-07-01 13:28:42 -0400

    Refactored Controllers for Custom Leaf Source

--- Sources/App/Modules/Blog/BlogFrontendController.swift
+++ Sources/App/Modules/Blog/BlogFrontendController.swift
@@ -26,7 +26,7 @@ struct BlogFrontendController {
 			.mapEach { Context.PostWithCategory(category: $0.category.viewContext, post: $0.viewContext) }
 			.flatMap {
 				let context = Context(title: "myPage - Blog", items: $0)
-				return req.view.render("blog", context)
+				return req.view.render("Blog/Frontend/Blog", context)
 		}
 	}
 	
@@ -53,7 +53,7 @@ struct BlogFrontendController {
                 let item = Context.PostWithCategory(category: post.category.viewContext,
                                                     post: post.viewContext)
                 let context = Context(title: "myPage - \("post.title")", item: item)
-                return req.view.render("post", context).encodeResponse(for: req)
+                return req.view.render("Blog/Frontend/Post", context).encodeResponse(for: req)
             }
     }
 }
