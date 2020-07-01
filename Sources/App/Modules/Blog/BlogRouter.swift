2245cafffd2f9cfd6eb3d29d6152080a14318dfe
Chad Rutherford <chad_rutherford@outlook.com>
2020-07-01 15:43:59 -0400

    Updated List.leaf to enable creation of new posts

--- Sources/App/Modules/Blog/BlogRouter.swift
+++ Sources/App/Modules/Blog/BlogRouter.swift
@@ -14,12 +14,14 @@ struct BlogRouter: RouteCollection {
 	func boot(routes: RoutesBuilder) throws {
 		routes.get("blog", use: self.frontendController.blogView)
 		routes.get(.anything, use: self.frontendController.postView)
-		routes.grouped([
+		let protected = routes.grouped([
 			UserModelSessionAuthenticator(),
-			// UserModel.guardMiddleware(),
 			UserModel.redirectMiddleware(path: "/")
 		])
-		.grouped("admin", "blog")
-			.get("posts", use: self.adminController.listView)
+		let blog = protected.grouped("admin", "blog")
+		let posts = blog.grouped("posts")
+		posts.get(use: self.adminController.listView)
+		posts.get("new", use: self.adminController.createView)
+		posts.post("new", use: self.adminController.create)
 	}
 }
